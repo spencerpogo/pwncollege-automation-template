028b34275da852492b2583c8eded5668c4121829
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-06-20 16:46:10 -0700

    Attempt pwntools hacks

--- challenges/challenge.py
+++ challenges/challenge.py
@@ -1,8 +1,6 @@
 from client import PwncollegeClient
 from abc import abstractclassmethod, abstractstaticmethod
 
-from pwnlib.tubes.ssh import ssh
-
 class Challenge:
     SSH_USER = "hacker"
     SSH_HOST = "dojo.pwn.college"
@@ -18,17 +16,6 @@ class Challenge:
     def start(self, client: PwncollegeClient, practice=False):
         client.start_docker(self.challenge_id, practice=practice)
     
-    def connect(self):
-        # work around bug
-        ssh.sftp = None
-        return ssh(user=self.SSH_USER, host=self.SSH_HOST, ssh_agent=True)
-    
     @abstractstaticmethod
     def exploit(tube):
         pass
-
-    @classmethod
-    def run(cls, client: PwncollegeClient):
-        cls.start(client)
-        tube = cls.connect()
-        cls.exploit(tube)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
 import sys
+import logging
+import inspect
 
 from client import PwncollegeClient
 from challenges import MODULES
 
 from pwn import *
 
+#logging.basicConfig(level=logging.DEBUG)
+#logging.getLogger("paramiko").setLevel(logging.DEBUG)
+#logging.getLogger("paramiko.transport").setLevel(logging.DEBUG)
 context.log_level = "DEBUG"
 
 def main():
@@ -36,9 +41,32 @@ def main():
         print("Logging in...")
         client.login(os.environ["PWNCOLLEGE_USERNAME"], os.environ["PWNCOLLEGE_PASSWORD"])
         print("Starting docker...")
-        challenge.start_docker(client)
+        challenge.start_docker(client, practice=True)
     print("Connecting via SSH...")
-    with challenge.connect():
+    # pwn.college specific HACK
+    def _hack_pwd(self):
+        d = self.run("pwd", tty=False).recvall().strip()
+        print("PWD!!", d)
+        return d
+    ssh.pwd = _hack_pwd
+
+    old_process = ssh.process
+    def _hack_process(self, argv=None, *a, **kw):
+        if argv == "false":
+            raise AssertionError("Don't try it! This hangs!")
+        return old_process(*a, **kw)
+    ssh.process = _hack_process
+
+    # hack for debug
+    _old_context_local = type(context).local
+    def _ctx_local_hack(*a, **kw):
+        if kw.get("log_level") == "error":
+            print("CAUGHT SNEAKY STUFF")
+            kw["log_level"] = "debug"
+        return _old_context_local(*a, **kw)
+    type(context).local = _ctx_local_hack
+
+    with ssh(user="hacker", host="dojo.pwn.college", ssh_agent=True) as tube:
         print("Running exploit...")
         challenge.exploit(tube)
 
