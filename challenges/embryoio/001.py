91869ced1b29e12e2e531939ab0cddde23df3743
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-06-20 18:44:53 -0700

    Create basic_challenge pattern

--- challenges/embryoio/001.py
+++ challenges/embryoio/001.py
@@ -1,11 +1,3 @@
-from ..challenge import Challenge
-from challenges import challenge
+from ..challenge import basic_challenge
 
-
-class Chall1(Challenge):
-    challenge_id = 1
-
-    def exploit(self, client):
-        r = client.invoke_shell()
-        r.sendline(b"/challenge/embryoio_level1; exit")
-        return r.recvall().strip().split(b"\n")[-1].decode()
+Chall1 = basic_challenge(1, b"/challenge/embryoio_level1; exit")
