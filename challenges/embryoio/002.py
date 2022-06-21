b628634b0938fe761b408cf6d436555fe167a0a6
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-06-20 18:50:31 -0700

    embryoio level 2

---
+++ challenges/embryoio/002.py
@@ -0,0 +1,3 @@
+from ..challenge import basic_challenge
+
+Chall2 = basic_challenge(2, b"echo dnwfwqyx | /challenge/embryoio_level2; exit")
--- challenges/embryoio/__init__.py
+++ challenges/embryoio/__init__.py
@@ -1,6 +1,6 @@
 from importlib import import_module
 
-NUM_CHALLS = 1
+NUM_CHALLS = 2
 CHALLENGES = [
     getattr(import_module("." + str(i).zfill(3), package=__name__), f"Chall{i}")()
     for i in range(1, NUM_CHALLS + 1)
